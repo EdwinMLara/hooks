975560e95b28482387c0d18b8e7fbd5b773173a7
EdwinMLara <emlara35@gmail.com>
2024-11-29 14:37:44 -0600

    Mostrando el funcionamiento de cada uno de los hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterUseState from './components/usestate/CounterUseState';
+import ToDoListUseState from './components/usestate/ToDoListUseState';
+import FormUseState from './components/usestate/FormUseState';
+import UserArrayUseState from './components/usestate/UserArrayUseState';
+import ThemeUseContext from './components/usecontext/ThemeUseContext';
+import InputUseRef from './components/useRef/InputUseRef';
+import PostUseEffect from './components/useeffect/PostUseEffect';
+import CounterActionUseReducer from './components/usereducer/CounterActionUseReducer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:"red"}}>UseState Examples</h1>
+      <CounterUseState/>
+      <p> ============================================================================ </p><br/>
+      <ToDoListUseState/>
+      <p> ============================================================================ </p><br/>
+      <FormUseState/>
+      <p> ============================================================================ </p><br/>
+      <UserArrayUseState/>
+      <h1 style={{color:"red"}}>UseContext Example</h1>
+      <ThemeUseContext/>
+      <h1 style={{color:"red"}}>Hook useref</h1>
+      <InputUseRef/>
+      <h1 style={{color:"red"}}>Hook useEffect</h1>
+      <PostUseEffect/>
+      <h1 style={{color:"red"}}>Hook useReduccer</h1>
+      <CounterActionUseReducer/>
     </div>
   );
 }
